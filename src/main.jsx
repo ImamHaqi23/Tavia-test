a521b8d35b83ff4fb7fdfdeea230b994dc061c8f
ImamBaihaqi <imam.baihaqi2396@gmail.com>
2023-12-10 08:40:53 +0700

    fix deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './store.js';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
